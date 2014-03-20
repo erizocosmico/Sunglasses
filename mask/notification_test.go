807f1f00e017adbde8bc92493ab39b9f0784b7f7
José Miguel Molina Arboledas <rd4091@gmail.com>
2014-03-20 23:22:33 +0100

    Tests for ListNotifications handler

--- mask/notification_test.go
+++ mask/notification_test.go
@@ -8,6 +8,7 @@ import (
 	"net/http/httptest"
 	"net/url"
 	"testing"
+	"fmt"
 )
 
 func TestSendNotification(t *testing.T) {
@@ -153,4 +154,126 @@ func TestMarkNotificationAsRead(t *testing.T) {
 	user.Remove(conn)
 	token.Remove(conn)
 	userTmp.Remove(conn)
+}
+
+func TestListNotifications(t *testing.T) {
+	var blankID bson.ObjectId
+	conn := getConnection()
+	users := make([]bson.ObjectId, 0, 24)
+	user, token := createRequestUser(conn)
+
+	for i := 0; i < 24; i++ {
+		u := NewUser()
+		u.Username = fmt.Sprintf("test_%d", i)
+		if err := u.Save(conn); err != nil {
+			panic(err)
+		}
+		u.Settings.Invisible = false
+		u.Settings.DisplayAvatarBeforeApproval = true
+		if err := u.Save(conn); err != nil {
+			panic(err)
+		}
+		users = append(users, u.ID)
+	}
+
+	for _, uid := range users {
+		if err := SendNotification(NotificationFollowed, user, blankID, uid, conn); err != nil {
+			panic(err)
+		}
+	}
+
+	Convey("Listing user's notifications", t, func() {
+		Convey("When invalid user is provided", func() {
+			testGetHandler(ListNotifications, func(r *http.Request) {}, conn, "/", "/",
+				func(resp *httptest.ResponseRecorder) {
+					var errResp errorResponse
+					if err := json.Unmarshal(resp.Body.Bytes(), &errResp); err != nil {
+						panic(err)
+					}
+					So(resp.Code, ShouldEqual, 403)
+					So(errResp.Code, ShouldEqual, CodeUnauthorized)
+					So(errResp.Message, ShouldEqual, MsgUnauthorized)
+				})
+		})
+
+		Convey("When no count params are passed", func() {
+			testGetHandler(ListNotifications, func(r *http.Request) {
+				r.Header.Add("X-User-Token", token.ID.Hex())
+			}, conn, "/", "/",
+				func(resp *httptest.ResponseRecorder) {
+					var errResp map[string]interface{}
+					if err := json.Unmarshal(resp.Body.Bytes(), &errResp); err != nil {
+						panic(err)
+					}
+					So(resp.Code, ShouldEqual, 200)
+					So(errResp["count"].(float64), ShouldEqual, float64(24))
+					So(len(errResp["notifications"].([]interface{})), ShouldEqual, 24)
+				})
+		})
+
+		Convey("When count param is passed", func() {
+			testGetHandler(ListNotifications, func(r *http.Request) {
+				r.Header.Add("X-User-Token", token.ID.Hex())
+				if r.Form == nil {
+					r.Form = make(url.Values)
+				}
+				r.Form.Add("count", "10")
+			}, conn, "/", "/",
+				func(resp *httptest.ResponseRecorder) {
+					var errResp map[string]interface{}
+					if err := json.Unmarshal(resp.Body.Bytes(), &errResp); err != nil {
+						panic(err)
+					}
+					So(resp.Code, ShouldEqual, 200)
+					So(errResp["count"].(float64), ShouldEqual, float64(10))
+					So(len(errResp["notifications"].([]interface{})), ShouldEqual, 10)
+				})
+		})
+
+		Convey("When count param and offset are passed", func() {
+			testGetHandler(ListNotifications, func(r *http.Request) {
+				r.Header.Add("X-User-Token", token.ID.Hex())
+				if r.Form == nil {
+					r.Form = make(url.Values)
+				}
+				r.Form.Add("count", "10")
+				r.Form.Add("offset", "15")
+			}, conn, "/", "/",
+				func(resp *httptest.ResponseRecorder) {
+					var errResp map[string]interface{}
+					if err := json.Unmarshal(resp.Body.Bytes(), &errResp); err != nil {
+						panic(err)
+					}
+					So(resp.Code, ShouldEqual, 200)
+					So(errResp["count"].(float64), ShouldEqual, float64(9))
+					So(len(errResp["notifications"].([]interface{})), ShouldEqual, 9)
+				})
+		})
+
+		Convey("When invalid count params are passed", func() {
+			testGetHandler(ListNotifications, func(r *http.Request) {
+				r.Header.Add("X-User-Token", token.ID.Hex())
+				if r.Form == nil {
+					r.Form = make(url.Values)
+				}
+				r.Form.Add("count", "2")
+			}, conn, "/", "/",
+				func(resp *httptest.ResponseRecorder) {
+					var errResp map[string]interface{}
+					if err := json.Unmarshal(resp.Body.Bytes(), &errResp); err != nil {
+						panic(err)
+					}
+					So(resp.Code, ShouldEqual, 200)
+					So(errResp["count"].(float64), ShouldEqual, float64(24))
+					So(len(errResp["notifications"].([]interface{})), ShouldEqual, 24)
+				})
+		})
+	})
+
+	conn.Db.C("notifications").RemoveAll(nil)
+	user.Remove(conn)
+	token.Remove(conn)
+	if _, err := conn.Db.C("users").RemoveAll(bson.M{"_id": bson.M{"$in": users}}); err != nil {
+		panic(err)
+	}
 }
