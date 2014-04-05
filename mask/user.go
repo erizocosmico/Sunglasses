75883658191ee5a759607dd76079f120ba010a03
José Miguel Molina Arboledas <rd4091@gmail.com>
2014-04-05 21:33:22 +0200

    Search implemented. Untested. Probably buggy.

--- mask/user.go
+++ mask/user.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"labix.org/v2/mgo/bson"
 	"strings"
+	"fmt"
+	"labix.org/v2/mgo"
 )
 
 // UserRole represents an user role
@@ -237,6 +239,88 @@ func (us UserSettings) GetPrivacySettings(objectType ObjectType) PrivacySettings
 	}
 }
 
+// Search is a handler for searching users
+func Search(c Context) {
+	var user User
+	count, offset := c.ListCountParams()
+
+	search := c.Form("q")
+	if c.User == nil || search == "" {
+		c.Error(400, CodeInvalidData, MsgInvalidData)
+		return
+	}
+
+	justFollowings := c.GetBoolean("just_followings")
+
+	getUserIter := func(count, offset int) mgo.Iter {
+		regex := bson.M{"$regex": fmt.Sprintf(`(?i)%s`, search)}
+
+		iter := c.Find("users", bson.M{"$and": []bson.M{
+			bson.M{"$or": []bson.M{
+				bson.M{"username": regex},
+				bson.M{"public_name": regex},
+				bson.M{"private_name": regex},
+			}},
+			bson.M{"active": true},
+			bson.M{"settings.invisible": false},
+		}}).Limit(count).Offset(offset).Iter()
+
+		return iter
+	}
+
+	users := make([]map[string]interface{}, 0, count)
+	for len(users) < count {
+		ids := make([]bson.ObjectId, 0, count)
+		iter := getUserIter(count, offset)
+		for iter.Next(&user) {
+			ids = append(ids, user.ID)
+		}
+
+		if justFollowings {
+			var follows []Follow
+			followsIter := c.Find("follows", bson.M{"user_from": user.ID, "user_to": bson.M{"$in": ids}}).Iter()
+
+			if err := followsIter.All(&follows); err != nil {
+				c.Error(500, CodeUnexpected, MsgUnexpected)
+				return
+			}
+
+			if len(follows) != len(ids) {
+				ids = make([]bson.ObjectId, 0, len(follows))
+				for _, f := range follows {
+					ids = append(ids, f.To)
+				}
+			}
+
+			if err := followsIter.Close(); err != nil {
+				c.Error(500, CodeUnexpected, MsgUnexpected)
+				return
+			}
+		}
+
+		userMap := GetUsersData(ids, c.User, c.Conn)
+		if userMap != nil {
+			for _, v := range userMap {
+				users = append(users, v)
+			}
+		}
+
+		if err := iter.Close(); err != nil {
+			c.Error(500, CodeUnexpected, MsgUnexpected)
+			return
+		}
+
+		if len(ids) == 0 {
+			break
+		}
+	}
+
+	c.Success(200, map[string]interface{
+			"count": len(users),
+			"users": users,
+	})
+}
+
 // GetUserData retrieves basic data from users for responses
 func GetUsersData(ids []bson.ObjectId, user *User, conn *Connection) map[bson.ObjectId]map[string]interface{} {
 	var u User
@@ -250,6 +334,10 @@ func GetUsersData(ids []bson.ObjectId, user *User, conn *Connection) map[bson.Ob
 	}}).Iter()
 
 	if err := followsIter.All(&follows); err != nil {
+		return nil
+	}
+
+	if err := followsIter.Close(); err != nil {
 		return nil
 	}
 
